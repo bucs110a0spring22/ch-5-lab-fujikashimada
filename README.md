84824d0839b18d61bc3187c615221dd4bc3e7de5
Fujiko Kashimada <98532804+fujikashimada@users.noreply.github.com>
2022-03-23 22:04:10 +0000

    finished part A,B,C

--- README.md
+++ README.md
@@ -8,18 +8,19 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Part A creates a dartboard and threw 10 darts, Part B involves 2 players to play the dart game giving back feedback on the results, Part C estimates pi by using the darts. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 4/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
-
+ none
 ## REFERENCES:
- < List any outside resources used >
-
+https://docs.python.org/3/library/random.html 
+https://docs.python.org/3/library/turtle.html#turtle.circle 
+https://docs.python.org/3/library/turtle.html#turtle.dot 
+https://docs.python.org/3/library/turtle.html#turtle.color
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ none
--- main.py
+++ main.py
@@ -31,7 +31,79 @@ import time
 #########################################################
 #                   Your Code Goes Below                #
 #########################################################
+def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
+  myturtle.up()
+  myturtle.goto(top_left_x, top_left_y)
+  myturtle.down()
+  for i in range (4):
+    myturtle.forward(width)
+    myturtle.right(90)
+  myturtle.up()
 
+def drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0):
+  myturtle.goto(x_start, y_start)
+  myturtle.down()
+  myturtle.goto(x_end,y_end)
+  myturtle.up()
+
+def drawCircle(myturtle=None, radius=0, step=None):
+  myturtle.goto(0,-1)
+  myturtle.down()
+  myturtle.circle(radius, 360, step)
+  myturtle.up()
+
+def setUpDartboard(myscreen=None, myturtle=None): 
+  myturtle.color("black")
+  turtle.setworldcoordinates(-2.5, -2.5, 2.5, 2.5)
+  drawSquare(myturtle, 2, -1, 1)
+  drawLine(myturtle=myturtle, x_start=-1.25, x_end=1.25)
+  drawLine(myturtle=myturtle, y_start=1.25, y_end=-1.25)
+  drawCircle(myturtle, 1, 100)
+
+def isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0):
+  if myturtle.distance(0,0)<=1:
+    return True
+  return isInCircle
+
+def throwDart(myturtle=None):  
+  myturtle.goto(random.uniform(-1, 1), random.uniform(-1,1))
+  myturtle.down()
+  if isInCircle(myturtle, 0, 0, 1)==True:
+    myturtle.color("green")
+    myturtle.dot(3,"green")
+    myturtle.up()
+  else:
+    myturtle.color("red")
+    myturtle.dot(3,"red")
+    myturtle.up()
+
+def playDarts(myturtle=None):
+  aPoints = 0
+  bPoints = 0
+  for i in range(10):
+    throwDart(myturtle)
+    if myturtle.color()[0]=="green":
+      aPoints += 1  
+    throwDart(myturtle)  
+    if myturtle.color()[0]=="green":
+      bPoints += 1
+  print("Player A, your final score is:", aPoints,"Player B, your final score is:", bPoints)
+  if aPoints > bPoints:
+    print("Congrats player A! You win!")
+  elif aPoints < bPoints:
+    print("Congrats player B! You win")
+  else:
+    print("It's a tie.")
+  
+# Part C
+def montePi(myturtle=None, num_darts=0):
+  insideCount = 0
+  for i in range(num_darts):
+    throwDart(myturtle)
+    if myturtle.color()[0]=="green":
+      insideCount += 1
+  approximatePi=insideCount/num_darts*4 
+  return approximatePi
 
 
 #########################################################

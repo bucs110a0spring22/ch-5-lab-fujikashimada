cd5ae0c43da2230775b3decea2298e8c71347554
Fujiko Kashimada <98532804+fujikashimada@users.noreply.github.com>
2022-04-05 03:14:21 +0000

    midterm, cleaned up and added functions

--- README.md
+++ README.md
@@ -1,26 +1,25 @@
 #### CS 110
-# Chapter 5 - Lab - Selection
-
-### [Assignment Description](https://docs.google.com/document/d/1QfPsRfo1kZoQw4p0DhjxZskNfE0eLAV6Z6SgPSleDM4/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- Part A creates a dartboard and threw 10 darts, Part B involves 2 players to play the dart game giving back feedback on the results, Part C estimates pi by using the darts. 
+What did you clean up?: Casted some variables and got rid of unessesary code. 
 
-## GRACE DAYS
-Grace days used for this assignment: 0
+Summary of function(s) added: The eye function when drawing the smiley face and a function that returns your age when you enter your birth year. 
 
-Grace days remaining: 4/5
+Summary of Feature Added: Draws a smiley face for the winner. 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- none
+none
+
 ## REFERENCES:
-https://docs.python.org/3/library/random.html 
+[ < List any outside resources used >](https://docs.python.org/3/library/random.html 
 https://docs.python.org/3/library/turtle.html#turtle.circle 
 https://docs.python.org/3/library/turtle.html#turtle.dot 
-https://docs.python.org/3/library/turtle.html#turtle.color
+https://docs.python.org/3/library/turtle.html#turtle.color)
+
 ## MISCELLANEOUS COMMENTS:
- none
+have a nice day:)
--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-'''
-Estimates pi using Monte Carlo simulation
-
-Virtual Dartboard has area 2 X 2 to accommodate unit circle
-Total area is 4
-Therefore, since area of unit circle = pi * radius^2 (and radius of 1 squared
-  is 1), ratio of area of unit circle to area of board should be pi/4
-  Theoretically, if you fill the entire board with darts, counting
-  the number of darts that fall within the circle divided by the
-  total number of darts thrown should give us that ratio (i.e., 1/4 * pi)
-  Therefore, multiplying that result by 4 should give us an approx. of pi
-
-Output to monitor:
-  approximation of pi (float)
-Output to window:
-  colored dots that simulate unit circle on 2x2 square
-Functions you must implement:
-  drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0) - to outline dartboard
-  drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0) - to draw axes
-  drawCircle(myturtle=None, radius=0) - to draw the circle
-  setUpDartboard(myscreen=None, myturtle=None) - to set up the board using the above functions
-  isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0) - determine if dot is in circle
-  throwDart(myturtle=None)
-  playDarts(myturtle=None) - a simulated game of darts between two players
-  montePi(myturtle=None, num_darts=0) - simulation algorithm returns the approximation of pi
-'''
 import turtle
 import random
 import time
 
-#########################################################
-#                   Your Code Goes Below                #
-#########################################################
+#draws the grid for the graph. 
 def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
   myturtle.up()
   myturtle.goto(top_left_x, top_left_y)
@@ -40,18 +12,21 @@ def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
     myturtle.right(90)
   myturtle.up()
 
+#draws the lines on the graph.
 def drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0):
   myturtle.goto(x_start, y_start)
   myturtle.down()
   myturtle.goto(x_end,y_end)
   myturtle.up()
 
+#draws the circle for the dart board. 
 def drawCircle(myturtle=None, radius=0, step=None):
   myturtle.goto(0,-1)
   myturtle.down()
   myturtle.circle(radius, 360, step)
   myturtle.up()
 
+#sets up the dartboard. 
 def setUpDartboard(myscreen=None, myturtle=None): 
   myturtle.color("black")
   turtle.setworldcoordinates(-2.5, -2.5, 2.5, 2.5)
@@ -65,6 +40,7 @@ def isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0):
     return True
   return isInCircle
 
+#throwing darts at the dartboard. 
 def throwDart(myturtle=None):  
   myturtle.goto(random.uniform(-1, 1), random.uniform(-1,1))
   myturtle.down()
@@ -77,6 +53,7 @@ def throwDart(myturtle=None):
     myturtle.dot(3,"red")
     myturtle.up()
 
+#calculating scores and returing winner. 
 def playDarts(myturtle=None):
   aPoints = 0
   bPoints = 0
@@ -95,7 +72,39 @@ def playDarts(myturtle=None):
   else:
     print("It's a tie.")
   
-# Part C
+#drawing a smiley face for the winner
+def smilesForWinners(myturtle=None):
+    myturtle = turtle.Turtle() 
+    window = turtle.Screen()
+    turtle.setworldcoordinates(-150, -150, 150, 150)
+    def eye(color, radian):
+      myturtle.down()
+      myturtle.fillcolor(color)
+      myturtle.begin_fill()
+      myturtle.circle(radian)
+      myturtle.end_fill()
+      myturtle.up()
+    # draw face
+    myturtle.width(2)
+    myturtle.fillcolor('yellow')
+    myturtle.begin_fill()
+    myturtle.circle(100)
+    myturtle.end_fill()
+    myturtle.up()
+    # draw eyes
+    myturtle.goto(-30, 115)
+    eye('black', 5)
+    myturtle.goto(30, 115)
+    eye('black', 5)
+    # draw mouth
+    myturtle.goto(-35, 85)
+    myturtle.down()
+    myturtle.right(90)
+    myturtle.circle(35, 180)
+    myturtle.up()
+    myturtle.clear()
+
+#Calculating Pi by throwing number of darts called by the user. 
 def montePi(myturtle=None, num_darts=0):
   insideCount = 0
   for i in range(num_darts):
@@ -105,27 +114,24 @@ def montePi(myturtle=None, num_darts=0):
   approximatePi=insideCount/num_darts*4 
   return approximatePi
 
+#asking user for birth year and returing their current age. 
+def age(birth_year=0):
+  current_year = 2022
+  age = current_year - birth_year
+  return age
 
-#########################################################
-#         Do not alter any code below here              #
-#       Your code must work with the main proivided     #
-#########################################################
 def main():
-    # Get number of darts for simulation from user
-    # Note continuation character <\> so we don't go over 78 columns:
     print("This is a program that simulates throwing darts at a dartboard\n" \
         "in order to approximate pi: The ratio of darts in a unit circle\n"\
         "to the total number of darts in a 2X2 square should be\n"\
         "approximately  equal to pi/4")
     print("=========== Part A ===========")
 
-    #Create window, turtle, set up window as dartboard
     window = turtle.Screen()
     darty = turtle.Turtle()
-    darty.speed(0) # as fast as it will go!
+    darty.speed(0) 
     setUpDartboard(window, darty)
 
-    # Loop for 10 darts to test your code
     for i in range(10):
         throwDart(darty)
     print("\tPart A Complete...")
@@ -133,23 +139,26 @@ def main():
     darty.clear()
     setUpDartboard(window, darty)
     playDarts(darty)
+    smilesForWinners(darty)
     print("\tPart B Complete...")
-    # Keep the window up until dismissed
     print("=========== Part C ===========")
     darty.clear()
     setUpDartboard(window, darty)
-    
-    # Includes the following code in order to update animation periodically
+
+      # Includes the following code in order to update animation periodically
     # instead of for each throw (saves LOTS of time):
     BATCH_OF_DARTS = 5000
     window.tracer(BATCH_OF_DARTS)
-
-    # Conduct simulation and print result
+ # Conduct simulation and print result
     number_darts = int(input("\nPlease input the number of darts to be thrown in the simulation:  "))
     approx_pi = montePi(darty, number_darts)
     print("\nThe estimation of pi using "+str(number_darts)+" virtual darts is " + str(approx_pi))
     print("\tPart C Complete...")
-    # Don't hide or mess with window while it's 'working'
+  
+    user_birth_year = int(input("\nPlease input the Year You Were Born In:")
+    calculatedAge = age(user_birth_year)
+    print("\nSince you were born in",     str(birth_year), "you are", str(calculatedAge), "years old")
+
     window.exitonclick()
 
 main()
